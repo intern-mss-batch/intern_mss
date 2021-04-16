d503659fc0e431f33626a16acff96a6be1e1c043
kmksampreeth <55936326+kmksampreeth@users.noreply.github.com>
2021-04-16 23:52:03 +0530

    Delete repository1/KOTHA M K SAMPREETH directory

--- repository1/KOTHA M K SAMPREETH/concates.java
+++
@@ -1,9 +0,0 @@
-package com.sampreeth.helloworld;
-
-public class concates {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello "+"how "+"are "+"you ");
-	}
-}
